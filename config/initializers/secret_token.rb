a94b7cc05b0610bea6f23fc3e7cdccf669ecbf92
priyankodey <priyankodey87@gmail.com>
2014-10-15 18:43:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biobody::Application.config.secret_key_base = '913ad47168a912f88c8a9d641df9ac6ed85b9718131004c941931783d6a8a8c9b12a44e5e82ff035bfe30326cad4900c897ccbaaf2706ecc2c2576e2c15eb5af'
